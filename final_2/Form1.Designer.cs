af6a557a1da0e011709803546b0bea601b206205
JoyBoy900908 <jeter20010908@gmail.com>
2024-09-26 10:58:50 +0800

    add

---
+++ final_2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace final_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.score = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Transparent;
+            this.ball.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ball.BackgroundImage")));
+            this.ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ball.Location = new System.Drawing.Point(330, 330);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(40, 40);
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            this.ball.Click += new System.EventHandler(this.ball_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.LimeGreen;
+            this.player.Location = new System.Drawing.Point(290, 370);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(120, 20);
+            this.player.TabIndex = 1;
+            this.player.UseVisualStyleBackColor = false;
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.LightCoral;
+            this.start.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.start.Location = new System.Drawing.Point(301, 189);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(96, 44);
+            this.start.TabIndex = 2;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.score.ForeColor = System.Drawing.Color.White;
+            this.score.Location = new System.Drawing.Point(12, 9);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(113, 31);
+            this.score.TabIndex = 3;
+            this.score.Text = "score：0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(242, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 45);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "A,D左右移動";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(595, 427);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.ball);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button player;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ final_2/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final_2
+{
+    public partial class Form1 : Form
+    {
+        int speed = 40;
+        int ballx = 10;//10
+        int bally = 20;//20
+        int sc,i;
+        int brickRowCount, brickColumnCount, brickPadding, brickOffsetTop, brickOffsetLeft;
+        bool gol=false, gor=false;
+        PictureBox[] brick = new PictureBox[48];
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                gol = true;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                gor = true;
+            }
+        }
+
+        private void ball_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                gol = false;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                gor = false;
+            }
+        }
+
+        private Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (gol && player.Left >=40)
+                player.Left -= speed;
+            if (gor && player.Left + player.Width <= 660)
+                player.Left += speed;
+            ball.Left += ballx;
+            ball.Top += bally;
+
+            if (ball.Left + ball.Width > 700 || ball.Left < 0)
+            {
+                ballx = -ballx; // this will bounce the object from the left or right border
+            }
+
+            if (ball.Top < 0 || ball.Bounds.IntersectsWith(player.Bounds))
+            {
+                bally = -bally; // this will bounce the object from top and bottom border
+            }
+
+            if (ball.Top > player.Top)
+            {
+                gameOver();
+                System.Windows.Forms.MessageBox.Show("Game Over", "提示");
+            }
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "brick")
+                {
+                    if (ball.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        this.Controls.Remove(x);
+                        bally = -bally;
+                        sc++;
+                    }
+                }
+            }
+            score.Text = "score：" + sc;
+            if (sc > 47)
+            {
+                gameOver();
+                System.Windows.Forms.MessageBox.Show("Game Success", "提示");
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            int rnd = rand.Next(0, 2);
+            if (rnd == 0)
+                ballx *= -1;
+
+            ball.Left = 330;
+            ball.Top = 330;
+            player.Left = 290;
+            player.Top = 370;
+            sc = 0;
+            brickRowCount = 3; 
+            brickColumnCount = 16;
+            brickOffsetLeft = 10;//10
+            brickOffsetTop = 50;//50
+            brickPadding = 1;
+            i = 0;
+            for (int c = 0; c < brickColumnCount; c++)
+            {
+                for (int r = 0; r < brickRowCount; r++)
+                {
+                    brick[i] = new PictureBox();
+                    brick[i].Size = new System.Drawing.Size(40, 25);
+                    brick[i].Location = new Point((c * (45 + brickPadding)) + brickOffsetLeft, (r * (45+ brickPadding)) + brickOffsetTop);
+                    brick[i].BringToFront();
+                    if(r==0)
+                        brick[i].BackColor = System.Drawing.Color.Yellow;
+                    else if(r==1)
+                        brick[i].BackColor = System.Drawing.Color.Blue;
+                    else if (r == 2)
+                        brick[i].BackColor = System.Drawing.Color.Green;
+                        
+                    brick[i].Tag = "brick";
+                    this.Controls.Add(brick[i]);
+                    i++;
+                }
+            }
+            start.Enabled = false;
+            start.Visible = false;
+            label1.Visible = false;
+            timer1.Start();
+        }
+        private void gameOver()
+        {
+            label1.Visible = true;
+            start.Enabled = true;
+            start.Visible = true;
+            timer1.Stop();
+        }
+    }
+}
